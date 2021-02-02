1ed872fa6dc8ee2e0e721a224cf0696055eb0428
Ahmad Hassan <ahmadh807@gmail.com>
2021-02-02 07:55:20 +0500

    useState Hooks

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,8 @@
+import { useState } from 'react';
 const Home = () => {
-  const handleShare = () => {
-    console.log("Shared successfully!");
-  }
-  const handlePrint = (name) => {
-    console.log('Hey!' + name);
+  const [greet, setGreet] = useState("Hey ✋");
+  const handleGreeting = () => {
+    setGreet("Peace out ✌️");
   }
     return (
       <div className="card mt-5">
@@ -11,23 +10,16 @@ const Home = () => {
           <h3 className="text-light">Welcome to React!</h3>
         </div>
         <div className="card-body">
+          <h4 className="text-center">{greet}</h4>
           <p>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique
             voluptas esse vel minus cupiditate earum culpa quam inventore
             accusantium rem impedit cumque quos et, tenetur eum quaerat
             obcaecati consequuntur tempora.
           </p>
-          <button className="btn btn-dark" onClick={handleShare}>
-            Share &rarr;
-          </button>
-          {/* Anonymous function */}
-          <button
-            className="btn btn-primary float-right"
-            onClick={() => {
-              handlePrint('Ahmad')
-            }}
-          >
-            Print
+
+          <button className="btn btn-dark" onClick={handleGreeting}>
+            Click me
           </button>
         </div>
         <div className="card-footer">
